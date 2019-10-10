f02af0cbeab14d595c83154567d50226aa1fccce
gaikwadgaurav997 <37701250+gaikwadgaurav997@users.noreply.github.com>
2019-10-10 16:32:01 +0530

    first angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
